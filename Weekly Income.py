a9fef64f4800122fbe2938bee56168397ec4c73a
Sachin-kr21 <107921455+Sachin-kr21@users.noreply.github.com>
2023-02-25 11:10:33 +0530

    Add files via upload

---
+++ Weekly Income.py
@@ -0,0 +1,4 @@
+x = eval(input("Enter Week Fraction:"))
+inc = int(input("Enter Income:"))
+y = eval(input("Enter Week Fraction to be solved:"))
+print("Income for the Week  Fraction is: " , (inc*y)/x )
